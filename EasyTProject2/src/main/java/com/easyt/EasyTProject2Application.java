da1f364310f933119a27f5da711878a5fe5fcd85
Lab1 <jvictorgmodesto@gmail.com>
2016-10-25 11:21:13 -0300

    cacaa

---
+++ EasyTProject2/src/main/java/com/easyt/EasyTProject2Application.java
@@ -0,0 +1,12 @@
+package com.easyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTProject2Application.class, args);
+	}
+}
